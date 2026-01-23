07f65cad80f0536d02709a2eb2d88778097c1ab9
hemasameerabandaru <hemasameera15@gmail.com>
2026-01-24 00:10:17 +0530

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone", // 👈 Add this line
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn-icons-png.flaticon.com', // If you used the icon URLs I gave earlier
+      },
+    ],
+  },
 };
 
 export default nextConfig;
